449ebc50f48e060fa74ad76914f286d2edbba38d
Am-2017 <d.anibal.romero@gmail.com>
2018-05-23 14:52:36 -0600

    Structure project
    add tree, structures and basic patches

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SonicPI
-Sonic Pi examples Files
+Sonic Pi tree
+
+	-MIDIs
+		imported patches from fl studio
+	-patches
+		attempt to build very basic EDM library for Sonic PI
+	-samples
+		import samples?
+	-test
+		1
+		...
---
+++ tests/1/firtBeat2.rb
@@ -0,0 +1,53 @@
+# Acid House with some boosting effects in the master bus
+# Mehackit 2016
+
+use_bpm 128
+
+with_fx :distortion, mix: 0.1 do
+  with_fx :nrhpf, mix: 0.06 do
+    
+    live_loop :bassorumpu do
+      sample :bd_tek, amp: 1.75
+      sleep 1
+    end
+    
+    live_loop :luuppibiitti do
+      sample :loop_compus, beat_stretch: 8, amp: 2
+      sleep 8
+    end
+    
+    
+    live_loop :bassoraita do
+      use_synth :hoover
+      use_synth_defaults release: rrand(0.05, 0.5), amp: rrand(1.5, 0.2)
+      with_fx :reverb, mix: 0.4 do
+        nuotit = (ring :C2, :C3, :r, :Eb3, :r, :G2, :Bb2, :r)
+        play nuotit.tick, cutoff: rrand(40, 120)
+      end
+      sleep 0.25
+    end
+    
+    
+    live_loop :hihat do
+      sample :drum_cymbal_pedal, amp: 0.15
+      sleep 0.25
+    end
+    
+    live_loop :virveli do
+      sleep 1
+      with_fx :reverb, mix: 0.5 do
+        sample :perc_snap, amp: 1.5
+      end
+      sleep 2
+    end
+    
+  end
+end
+
+
+
+
+
+
+
+
